e52ff696ef82c94f143139cde8cc06b6a556f3d1
Jon Penton <hey@jondpenton.com>
2020-10-21 17:28:39 -0400

    feat: :sparkles: finished exercise 1

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,25 +1,17 @@
-// 🐨 make sure to add the comment and import jsx from @emotion/core
-// up here so you can use the css prop
-
-// 🐨 let's get a solid reset of global styles so everything looks a bit better
-// In this project we're using bootstrap-reboot which you can import from
-// bootstrap/dist/css/bootstrap-reboot.css
-// 🦉 Note: you can definitely use regular styles to style React apps
-// and using any modern toolchain will allow you to simply import the CSS file
-// but CSS-in-JS is generally easier to maintain.
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core'
 import '@reach/dialog/styles.css'
+import 'bootstrap/dist/css/bootstrap-reboot.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
-// 🐨 you'll need to import some new components that you'll be creating
-// in this file
-// import {Button, Input, FormGroup} from './components/lib'
-import {Modal, ModalContents, ModalOpenButton} from './components/modal'
-import {Logo} from './components/logo'
+import { Button, FormGroup, Input } from './components/lib'
+import { Logo } from './components/logo'
+import { Modal, ModalContents, ModalOpenButton } from './components/modal'
 
-function LoginForm({onSubmit, submitButton}) {
+function LoginForm({ onSubmit, submitButton }) {
   function handleSubmit(event) {
     event.preventDefault()
-    const {username, password} = event.target.elements
+    const { username, password } = event.target.elements
 
     onSubmit({
       username: username.value,
@@ -27,29 +19,29 @@ function LoginForm({onSubmit, submitButton}) {
     })
   }
 
-  // 🐨 this <form> could use a css prop
-  // 🎨
-  //    display: 'flex',
-  //    flexDirection: 'column',
-  //    alignItems: 'stretch',
-  //    '> div': {
-  //      margin: '10px auto',
-  //      width: '100%',
-  //      maxWidth: '300px',
-  //    },
   return (
-    <form onSubmit={handleSubmit}>
-      {/* 🐨 these div elements could be a FormGroup you create in components/lib */}
-      {/* 🐨 and the inputs elements could be custom styled Input components too */}
-      <div>
+    <form
+      onSubmit={handleSubmit}
+      css={css`
+        display: flex;
+        flex-direction: column;
+        align-items: stretch;
+        > div {
+          margin: 10px auto;
+          width: 100%;
+          max-width: 300px;
+        }
+      `}
+    >
+      <FormGroup>
         <label htmlFor="username">Username</label>
-        <input id="username" />
-      </div>
-      <div>
+        <Input id="username" />
+      </FormGroup>
+      <FormGroup>
         <label htmlFor="password">Password</label>
-        <input id="password" type="password" />
-      </div>
-      <div>{React.cloneElement(submitButton, {type: 'submit'})}</div>
+        <Input id="password" type="password" />
+      </FormGroup>
+      <div>{React.cloneElement(submitButton, { type: 'submit' })}</div>
     </form>
   )
 }
@@ -63,46 +55,45 @@ function App() {
     console.log('register', formData)
   }
 
-  // 🐨 this div could use a css prop to get its children rendered nicer
-  // 🎨
-  //    display: 'flex',
-  //    flexDirection: 'column',
-  //    alignItems: 'center',
-  //    justifyContent: 'center',
-  //    width: '100%',
-  //    height: '100vh',
   return (
-    <div>
+    <div
+      css={css`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100vh;
+      `}
+    >
       <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
-      {/*
-        🐨 the two buttons are too close, let's space them out
-          🎨 apply this to the div right below
-            display: 'grid',
-            gridTemplateColumns: 'repeat(2, minmax(0, 1fr))',
-            gridGap: '0.75rem',
-      */}
-      {/* 🐨 And make sure to use the new Button component for all these buttons */}
-      <div>
+      <div
+        css={css`
+          display: grid;
+          grid-template-columns: repeat(2, minmax(0, 1fr));
+          grid-gap: 0.75rem;
+        `}
+      >
         <Modal>
           <ModalOpenButton>
-            <button variant="primary">Login</button>
+            <Button variant="primary">Login</Button>
           </ModalOpenButton>
           <ModalContents aria-label="Login form" title="Login">
             <LoginForm
               onSubmit={login}
-              submitButton={<button variant="primary">Login</button>}
+              submitButton={<Button variant="primary">Login</Button>}
             />
           </ModalContents>
         </Modal>
         <Modal>
           <ModalOpenButton>
-            <button variant="secondary">Register</button>
+            <Button variant="secondary">Register</Button>
           </ModalOpenButton>
           <ModalContents aria-label="Registration form" title="Register">
             <LoginForm
               onSubmit={register}
-              submitButton={<button variant="secondary">Register</button>}
+              submitButton={<Button variant="secondary">Register</Button>}
             />
           </ModalContents>
         </Modal>
